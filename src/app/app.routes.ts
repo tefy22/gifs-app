3e754a59fd9d610109e3de83e93a3e2477356cfc
Stefania Afanador Ortega <stefaniasafanadoro@interrapidisimo.com>
2026-09-03 17:23:24 -0500

    avance

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DashoardPage } from './gifs/pages/dashoard-page/dashoard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'dashboard',
+        component: DashoardPage
+    }
+
+
+];
---
+++ src/app/gifs/pages/dashoard-page/dashoard-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashoard-page',
+  imports: [],
+  templateUrl: './dashoard-page.html',
+})
+export class DashoardPage {}
