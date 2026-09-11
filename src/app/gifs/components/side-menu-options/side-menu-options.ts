7d82c5cc9cbb2015d40e2ee6b3fdaa1150d14003
Stefania Afanador Ortega <stefaniasafanadoro@interrapidisimo.com>
2026-09-11 16:04:47 -0500

    avance 11/09 4:00
    avance en el trabajo

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gift-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
 export class SideMenuOptions {
